cc2e58bbfcba2c91fb9667ab2d9538023d968f1d
Xancake <xanylein@web.de>
2015-11-08 23:52:47 +0100

    Persistence-API gelöscht, da etwas Neues entwickelt wird

--- Projekt_XancakeLib/src/de/xancake/io/persistence/Loader_I.java
+++
@@ -1,8 +0,0 @@
-package de.xancake.io.persistence;
-
-import java.io.IOException;
-
-public interface Loader_I<T> {
-	
-	T load(int id) throws IOException;
-}
--- Projekt_XancakeLib/src/de/xancake/io/persistence/Storer_I.java
+++
@@ -1,8 +0,0 @@
-package de.xancake.io.persistence;
-
-import java.io.IOException;
-
-public interface Storer_I<T> {
-	
-	void store(T object) throws IOException;
-}
