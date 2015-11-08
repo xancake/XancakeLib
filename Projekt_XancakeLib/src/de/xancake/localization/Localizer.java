868fc84ee164292b9b0f7438d8f138ee1e5b7d8e
Xancake <xanylein@web.de>
2015-11-09 00:46:11 +0100

    set-Methode für die Localizer-Quellen entfernt

--- Projekt_XancakeLib/src/de/xancake/localization/Localizer.java
+++ Projekt_XancakeLib/src/de/xancake/localization/Localizer.java
@@ -4,41 +4,34 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.Objects;
 import de.xancake.localization.source.LocalizationSource_I;
 
 public class Localizer {
-	private List<LocalizationSource_I> myLocalizationSources;
+	private List<LocalizationSource_I> _sources;
 	
 	public Localizer() {
 		this(new ArrayList<LocalizationSource_I>());
 	}
 	
 	public Localizer(List<LocalizationSource_I> sources) {
-		setLocalizationSources(sources);
+		_sources = Objects.requireNonNull(sources);
 	}
 	
 	public boolean hasLocalizationSources() {
-		return !myLocalizationSources.isEmpty();
+		return !_sources.isEmpty();
 	}
 	
 	public List<LocalizationSource_I> getLocalizationSources() {
-		return Collections.unmodifiableList(myLocalizationSources);
-	}
-	
-	public void setLocalizationSources(List<LocalizationSource_I> sources) {
-		if(sources == null) {
-			throw new IllegalArgumentException("The Sources-List may not be null!");
-		}
-		myLocalizationSources = sources;
+		return Collections.unmodifiableList(_sources);
 	}
 	
 	public void addLocalizationSource(LocalizationSource_I source) {
-		myLocalizationSources.add(source);
+		_sources.add(source);
 	}
 	
 	public void clearLocalizationSources() {
-		myLocalizationSources.clear();
+		_sources.clear();
 	}
 	
 	public String localize(String key, Object... values) {
@@ -46,7 +39,7 @@ public class Localizer {
 	}
 	
 	private String fetchTranslation(String key) {
-		for(LocalizationSource_I source : myLocalizationSources) {
+		for(LocalizationSource_I source : _sources) {
 			if(source.containsKey(key)) {
 				return source.getValue(key);
 			}
